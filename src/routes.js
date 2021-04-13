a30c66bcb54fed396c23dec60b18a12644b7cab9
David Prenninger <david.prenninger@gmx.de>
2021-04-13 18:34:56 +0200

    Adding multi request endpoint for test purposes

--- src/routes.js
+++ src/routes.js
@@ -53,7 +53,7 @@ export default (app) => {
       return next(error);
     }
     // Request route from graphhopper
-    axios.get(`${graphhopper.url}${graphhopper.route}?locale=de&point=${from.join(',')}&point=${to.join(',')}&details=road_class&details=max_speed&instructions=true`)
+    axios.get(`${graphhopper.url}${graphhopper.route}?locale=de&point=${from.join(',')}&point=${to.join(',')}&details=road_class&details=max_speed&instructions=true&profile=cargobike`)
       .then((response) => {
         res.send(response.data)
       })
@@ -121,7 +121,7 @@ export default (app) => {
       return next(error);
     }
     // Request route from graphhopper
-    axios.get(`${graphhopper.url}${graphhopper.route}?locale=de&point=${from.join(',')}&point=${to.join(',')}&points_encoded=false&details=road_class&details=max_speed&instructions=true`)
+    axios.get(`${graphhopper.url}${graphhopper.route}?locale=de&point=${from.join(',')}&point=${to.join(',')}&points_encoded=false&details=road_class&details=max_speed&instructions=true&&profile=cargobike`)
       .then((response) => {
         res.send(directionsMapper.getMapping(
           response.data,
@@ -130,6 +130,96 @@ export default (app) => {
           access_token,
           'polyline6'
         ));
+      })
+      .catch((error) => {
+        error.status = 500;
+        error.description = 'Network request failed!';
+        next(error);
+      })
+  })
+
+  /**
+   * @openapi
+   * /routes:
+   *   get:
+   *     title: Bikerouting / Cargobike routing
+   *     description: Formatted in Mapbox-navigation format.
+   *     parameters:
+   *       - in: path
+   *         name: from
+   *         schema:
+   *           type: array
+   *           items:
+   *             type: float
+   *           minItems: 2
+   *           maxItems: 2
+   *         required: true
+   *         description: Latitude and longitude of start location
+   *       - in: path
+   *         name: to
+   *         schema:
+   *           type: array
+   *           items:
+   *             type: float
+   *           minItems: 2
+   *           maxItems: 2
+   *         required: true
+   *         description: Latitude and longitude of destination location
+   *       - in: path
+   *         name: access_token
+   *         schema:
+   *           type: string
+   *         required: true
+   *         description: MapBox Access token
+   * 
+   *     responses:
+   *       200:
+   *         description: 'Routing Result'
+   *         content:
+   *           application/json
+   */
+   app.get('/route', (req, res, next) => {
+    // Check Parameters
+    let from;
+    let to;
+    let access_token;
+    try {
+      from = required(req, 'from', 'array', { minLength: 2, maxLength: 2 });
+      to =  required(req, 'to', 'array', { minLength: 2, maxLength: 2 });
+      access_token =  required(req, 'access_token', 'string');
+    } catch (error) {
+      error.status = 400;
+      error.description = error.message;
+      return next(error);
+    }
+    // Request route from graphhopper
+    axios.get(`${graphhopper.url}${graphhopper.route}?locale=de&point=${from.join(',')}&point=${to.join(',')}&points_encoded=false&details=road_class&details=max_speed&instructions=true&profile=bike`)
+      .then((response) => {
+        const bikeRoute = directionsMapper.getMapping(
+          response.data,
+          undefined,
+          'de',
+          access_token,
+          'polyline6'
+        );
+        axios.get(`${graphhopper.url}${graphhopper.route}?locale=de&point=${from.join(',')}&point=${to.join(',')}&points_encoded=false&details=road_class&details=max_speed&instructions=true&profile=cargobike`)
+          .then((response) => {
+            res.send({
+              bike: bikeRoute,
+              cargobike: directionsMapper.getMapping(
+                response.data,
+                undefined,
+                'de',
+                access_token,
+                'polyline6'
+              ),
+            });
+          })
+          .catch((error) => {
+            error.status = 500;
+            error.description = 'Network request failed!';
+            next(error);
+          })
       })
       .catch((error) => {
         error.status = 500;
